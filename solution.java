2f5c1bfc28f9c22538689450e9a8a70966ebe67e
srishti-vashisht <52890038+srishti-vashisht@users.noreply.github.com>
2022-07-26 21:05:00 +0530

    2nd day

---
+++ solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int original=x;
+        int rev=0;
+        
+        while(x>0){
+            int rem=x%10;
+            rev=rev*10+rem;
+            x=x/10;
+            
+        }
+        if(original==rev)
+        
+            return true;
+        else
+            
+            return false;      
+    }
+}
